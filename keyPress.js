bcf2cbadae6f9c80a9fbf8bfb55b6a46b78ecb17
Andy Buck <brotherandy@live.com>
2021-09-03 19:37:47 -0400

    remove yucpic, style changes

--- keyPress.js
+++ keyPress.js
@@ -1,43 +1,27 @@
 function keyPressed(){
   if (game.mode === "play" && !game.paused && !world.noKeys && !man.isAnimated && !window.event.shiftKey){//code is also in game.js
     switch(keyCode){
-      case LEFT_ARROW:
-        active.move(-1, 0)
-        break
-      case RIGHT_ARROW:
-        active.move(1,0)
-        break
-      case UP_ARROW:
-        active.move(0,-1)
-        break
-      case DOWN_ARROW:
-        active.move(0,1)
-        break
-      default:
-        game.action(key)
+      case LEFT_ARROW:  active.move(-1, 0); break;
+      case RIGHT_ARROW: active.move(1,0);   break;
+      case UP_ARROW:    active.move(0,-1);  break;
+      case DOWN_ARROW:  active.move(0,1);   break;
+      default:          game.action(key);
     }
   }
   else if (["dumpMenu", "build"].includes(popup.type)){
     switch(keyCode){
       case UP_ARROW:
       case LEFT_ARROW:
-        popup.changeSelect(-1)
-        return false
+        popup.changeSelect(-1);  return false
       case DOWN_ARROW:
       case RIGHT_ARROW:
-        popup.changeSelect(1)
-        return false
+        popup.changeSelect(1);   return false
     }
   }
   switch(keyCode){
-    case ENTER:
-      $('#etr').click()
-      break
-    case ESCAPE:
-      $('#esc').click()
-      break
-    case 32:
-      game.pauseGame()
+    case ENTER:  $('#etr').click();   break;
+    case ESCAPE: $('#esc').click();   break;
+    case 32:     game.pauseGame();
   }
 }
 
