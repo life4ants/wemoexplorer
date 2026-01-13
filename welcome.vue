702bc5fe2f465f35cb61124bf91531b1282c2643
Andy Buck <brotherandy@live.com>
2026-01-13 16:33:45 -0600

    version 1.5.1

--- welcome.vue
+++ welcome.vue
@@ -27,6 +27,14 @@
           <h5>Version {{version}}</h5>
           <h6>Published {{publicationDate}}</h6>
           <ul>
+            <li>start in tutorial</li>
+            <li>new object: mushrooms</li>
+          </ul>
+        </div>
+        <div class="whatsNew">
+          <h5>Version 1.5.0</h5>
+          <h6>Published Jan 8, 2026</h6>
+          <ul>
             <li>find stars to reveal clouds</li>
           </ul>
         </div>
@@ -102,8 +110,8 @@ module.exports = {
       customWorlds: [],
       name: "",
       deleteMode: false,
-      version: "1.5.0",
-      publicationDate: "Jan 8, 2026",
+      version: "1.5.1",
+      publicationDate: "Jan 13, 2026",
       pageViews: "loading"
     }
   },
